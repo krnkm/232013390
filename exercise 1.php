546ca3d9efce48064eb0790658899949eb925c52
krnkm <153052127+krnkm@users.noreply.github.com>
2025-03-06 16:37:17 +0800

    finished exercise 1

---
+++ exercise 1.php
@@ -0,0 +1,20 @@
+<?php
+$a = 15;
+$b = 7;
+
+// Sum
+$sum = $a + $b;
+echo "Sum: " . $sum . "\n";
+
+// Difference
+$difference = $a - $b;
+echo "Difference: " . $difference . "\n";
+
+// Product
+$product = $a * $b;
+echo "Product: " . $product . "\n";
+
+// Quotient
+$quotient = $a / $b;
+echo "Quotient: " . $quotient . "\n";
+?>
