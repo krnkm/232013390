1dbcffce3c81698d5ce1c247717a292ef06efcbc
krnkm <153052127+krnkm@users.noreply.github.com>
2025-03-06 16:40:16 +0800

    finished exercise 2

---
+++ exercise 2.php
@@ -0,0 +1,21 @@
+<?php
+$number = 10;
+
+if ($number % 2 == 0) {
+    echo "$number is Even.\n";
+} else {
+    echo "$number is Odd.\n";
+}
+?>
+
+<?php
+$number = 10;
+
+if ($number > 0) {
+    echo "$number is Positive.\n";
+} elseif ($number < 0) {
+    echo "$number is Negative.\n";
+} else {
+    echo "$number is Zero.\n";
+}
+?>
